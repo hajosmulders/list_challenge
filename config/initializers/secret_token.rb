ab43caafa0a34f0e0a968e8e1a8a823d1191170d
Henri Smulders <hajosmulders@gmail.com>
2013-12-07 17:19:16 +0000

    rails app with mysql initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ListChallenge::Application.config.secret_key_base = '1f7447bcdea7e8b406c3067a04a9b07c6d87db6278173af54279f87312c69d31ac45f9fe7e274bd4f1ddc824094fa9c9be7cc6a54767fe6b1ed3b715caa0ca09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ListChallenge::Application.config.session_store :cookie_store, key: '_list_challenge_session'
