a0e8fc98eebe132bb3ee4a53b229b13695dc40bd
Henri Smulders <hajosmulders@gmail.com>
2013-12-07 18:19:16 +0000

    root route and index (enter_id) set

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ListChallenge::Application.routes.draw do
-  get "frontpage/enter_id"
+  resources :clocker
+  root "frontpage#enter_id"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +56,3 @@ ListChallenge::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
