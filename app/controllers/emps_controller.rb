bdd3acb11acf2ab827bcd4efa9bad014b40139fe
Henri Smulders <hajosmulders@gmail.com>
2013-12-08 18:52:44 +0000

    Now with working employee form for new employees and display of existing emp

---
+++ app/controllers/emps_controller.rb
@@ -0,0 +1,17 @@
+class EmpsController < ApplicationController
+  def create
+    @emp = Emp.new(emp_params)
+
+    @emp.save
+    redirect_to @emp
+  end
+  
+  def show
+    @emp = Emp.find(params[:id])
+  end
+  
+  private
+  def emp_params
+    params.require(:emp).permit(:first_name, :last_name, :role, :emp_id)
+  end
+end
--- app/controllers/empscontroller.rb
+++
@@ -1,2 +0,0 @@
-class EmpsController < ApplicationController
-end
